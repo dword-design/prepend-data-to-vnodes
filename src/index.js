66d9301283c16db5fcf924f9bf2b9d6a082f6521
Sebastian Landwehr <info@dword-design.de>
2019-05-14 12:16:50 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { forIn } from '@dword-design/functions'
+import { mergeData } from 'vue-functional-data-merge'
+
+export default (vnodes, data) => {
+  (vnodes ?? []) |> forIn(node => node.data = mergeData(data, node.data ?? {}))
+  return vnodes
+}
